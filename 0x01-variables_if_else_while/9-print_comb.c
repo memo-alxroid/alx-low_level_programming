4fb0912b7db662962ed0d20fcac2853e4584ddfc
memo-alxroid <142922525+memo-alxroid@users.noreply.github.com>
2023-09-23 23:25:34 +0300

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,1 +1,30 @@
+#include <stdio.h>
 
+/**
+ * main - print the size of various types
+ *
+ * Description: all possible combinations of single-digit numbers
+ * starting from 0
+ *
+ *
+ * Return: Allways 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	int base10 = 0;
+
+	while (base10 < 10)
+	{
+		putchar(48 + base10);
+    if (base10 != 9)
+    {
+        putchar(',');
+        putchar(' ');
+    }
+    base10++;
+	}
+	putchar('\n');
+	return (0);
+}
