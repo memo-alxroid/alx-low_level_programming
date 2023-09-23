e39a986ff79314b77fcb5cbbcc4e35ac850079f3
Mina Sami <memo.samy0050@gmail.com>
2023-09-23 13:01:41 -0700

    prints all single digit numbers of base 10 starting from 0

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - print the size of various types
+ *
+ * Description: prints all single digit numbers of base 10
+ * starting from 0
+ *
+ *
+ * Return: Allways 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	int base10 = 0;
+
+	while (base10 < 10)
+	{
+		printf("%d", base10);
+		base10++;
+	}
+	putchar('\n');
+	return (0);
+}
