76ae8e6d70e41d24b68b8e5efc2595a376df6b2c
memo-alxroid <142922525+memo-alxroid@users.noreply.github.com>
2023-09-23 23:19:15 +0300

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,1 +1,31 @@
+#include <stdio.h>
 
+/**
+ * main - print the size of various types
+ *
+ * Description: prints all the numbers of base 16 in lowercase
+ * starting from 0
+ *
+ *
+ * Return: Allways 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	int base16 = 0;
+  char alpha = 'a';
+
+	while (base16 < 10)
+	{
+		putchar(48 + base16);
+		base16++;
+	}
+  while (alpha <= 'f')
+	{
+		putchar(alpha);
+		alpha++;
+	}
+	putchar('\n');
+	return (0);
+}
