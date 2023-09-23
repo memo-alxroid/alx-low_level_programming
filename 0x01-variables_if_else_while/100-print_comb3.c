4116a87bbc1fcff8348e64aadc4145cf6518e549
memo-alxroid <142922525+memo-alxroid@users.noreply.github.com>
2023-09-23 23:48:59 +0300

    all possible different combinations of two digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,1 +1,37 @@
+#include <stdio.h>
 
+/**
+ * main - print the size of various types
+ *
+ * Description: all possible combinations of single-digit numbers
+ * starting from 0
+ *
+ *
+ * Return: Allways 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	int firstDigit = 0;
+  int secondDigit;
+
+	while (firstDigit < 9)
+	{
+    secondDigit = firstDigit + 1;
+		putchar(48 + base10);
+    while (secondDigit < 10)
+      {
+        putchar(48 + base10);
+        if (firstDigit != 8 && secondDigit != 9)
+        {
+             putchar(',');
+			       putchar(' ');
+        }
+        secondDigit++;
+      }
+		firstDigit++;
+	}
+	putchar('\n');
+	return (0);
+}
