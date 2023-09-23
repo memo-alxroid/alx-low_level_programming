bb8d6271d288c43461365f08de02376cdc5c472f
memo-alxroid <142922525+memo-alxroid@users.noreply.github.com>
2023-09-23 23:09:07 +0300

    Update 7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
 
+/**
+ * main - print the size of various types
+ *
+ * Description: prints the alphabet in lowercase in reverse
+ *
+ *
+ * Return: Allways 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	char alpha = 'z';
+
+	while (alpha >= 'a')
+	{
+		putchar(alpha);
+		alpha--;
+	}
+	putchar('\n');
+	return (0);
+}
