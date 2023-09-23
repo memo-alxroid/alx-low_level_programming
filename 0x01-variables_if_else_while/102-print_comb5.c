71fa8655a4160c4f4c7f792e8799537870fc9d83
memo-alxroid <142922525+memo-alxroid@users.noreply.github.com>
2023-09-24 00:13:27 +0300

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,1 +1,45 @@
 
+#include <stdio.h>
+
+/**
+ * main - print the size of various types
+ *
+ * Description: all possible combinations of two two-digit numbers
+ * starting from 0
+ *
+ *
+ * Return: Allways 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	int firstDigit = 0;
+	int secondDigit;
+	int thirdDigit;
+
+	while (firstDigit < 8)
+	{
+		secondDigit = firstDigit + 1;
+		while (secondDigit < 9)
+		{
+			thirdDigit = secondDigit + 1;
+			while (thirdDigit < 10)
+			{
+				putchar(48 + firstDigit);
+				putchar(48 + secondDigit);
+				putchar(48 + thirdDigit);
+				if (firstDigit != 7)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+				thirdDigit++;
+			}
+			secondDigit++;
+		}
+		firstDigit++;
+	}
+	putchar('\n');
+	return (0);
+}
