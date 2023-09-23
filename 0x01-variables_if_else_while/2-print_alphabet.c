c5f47466802ffd848a06d148689624eb29c96c62
Mina Sami <memo.samy0050@gmail.com>
2023-09-23 12:31:55 -0700

    prints the alphabet in lowercase, followed by a new line

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - print the size of various types
+ *
+ * Description: prints the alphabet in lowercase 
+ *
+ *
+ * Return: Allways 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	char alpha = 'a';
+ 	while (alpha <= 'z')
+	{
+		putchar(alpha);
+		alpha++;
+	}
+	putchar('\n');
+	return (0);
+}
