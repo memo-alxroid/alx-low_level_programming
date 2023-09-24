7b04417084669b88ef9d71982ad2d6e9e35eede1
Mina Sami <memo.samy0050@gmail.com>
2023-09-24 10:37:52 -0700

    prints _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,17 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
- * main - prints Putchar
+ * main - Entry Point
  *
  * Description: prints _putchar
  *
- * Return: 0 (Success)
+ *
+ * Return: Allways 0 (Success)
+ *
  */
+
+
 
 int main(void)
 {
@@ -19,6 +24,5 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-
 	return (0);
 }
