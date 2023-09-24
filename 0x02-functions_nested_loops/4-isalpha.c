1221cc3a61c8dfae22764cf8ed292b5d6bd74525
Mina Sami <memo.samy0050@gmail.com>
2023-09-24 11:40:11 -0700

    checks for alphabetic character

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * _isalpha - Entry Point
+ * @c: the charchter to check
+ *
+ * Description: checks for alphabetic character
+ *
+ *
+ * Return: 1 if c is a letter 0 otherwise
+ *
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
