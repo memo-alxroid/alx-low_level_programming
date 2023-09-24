689ed5f76402015e4416526ca1e27e69520554f4
memo-alxroid <142922525+memo-alxroid@users.noreply.github.com>
2023-09-24 20:55:03 +0300

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,6 +11,28 @@
  *
  */
 
+/**
+ * print_alphabet - prints the alphabet in lowercase
+ *
+ * Description: prints the alphabet in lowercase
+ *
+ *
+ * Return: Allways 0 (Success)
+ *
+ */
+
+void print_alphabet(void)
+{
+	char alpha = 'a';
+
+	while (alpha <= 'z')
+	{
+		_putchar(alpha);
+		alpha++;
+	}
+	_putchar('\n');
+}
+
 void print_alphabet_x10(void)
 {
   int i = 0;
