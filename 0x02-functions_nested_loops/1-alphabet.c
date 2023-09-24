df7b1254fb326b14b03f5492f235b9efefb649b3
memo-alxroid <142922525+memo-alxroid@users.noreply.github.com>
2023-09-24 20:41:16 +0300

    prints the alphabet, in lowercase

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
+#include "main.h"
 
+/**
+ * main - print the size of various types
+ *
+ * Description: prints the alphabet in lowercase
+ *
+ *
+ * Return: Allways 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	char alpha = 'a';
+
+	while (alpha <= 'z')
+	{
+		_putchar(alpha);
+		alpha++;
+	}
+	_putchar('\n');
+	return (0);
+}
