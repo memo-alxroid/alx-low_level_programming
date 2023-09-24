2f47c7fd2908896c91fb826b02ca4e51bf72408c
Mina Sami <memo.samy0050@gmail.com>
2023-09-24 12:53:10 -0700

    prints the last digit of a number

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_last_digit - Entry Point
+ * @n: number to get the last digit from
+ *
+ * Description: prints the last digit of a number
+ *
+ *
+ * Return: the value of the last digit
+ *
+ */
+
+int print_last_digit(int n)
+{
+	int divisor;
+	int lastDigit;
+
+	divisor = n / 10;
+	divisor = divisor * 10;
+	lastDigit = n - divisor;
+	return (lastDigit);
+}
