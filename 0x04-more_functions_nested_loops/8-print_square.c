30bfaffa7067643f61774dcea1492bc512e70730
Mina Sami <memo.samy0050@gmail.com>
2023-09-26 13:54:50 -0700

    prints a square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* print_square - draws a square
+* @size: the size of the square
+*
+* Description:  draws a square
+*
+*
+* Return: void
+*
+*/
+
+void print_square(int size)
+{
+	int i;
+	int j;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
