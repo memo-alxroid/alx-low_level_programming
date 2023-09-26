f0beec1ff91fa542116021fef797a0de2444091e
Mina Sami <memo.samy0050@gmail.com>
2023-09-26 14:10:06 -0700

    Fizz-Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* main - Entry Point
+*
+* Description: prints the numbers from 1 to 100
+* for multiples of three print Fizz
+* for the multiples of five print Buzz
+* multiples of both three and five print FizzBuzz
+*
+* Return: 0 success
+*
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i < 101; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+	printf("\n");
+	return (0);
+}
