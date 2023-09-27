bcbccae472e5957407a695ff574068bb7bc5f6b6
Mina Sami <memo.samy0050@gmail.com>
2023-09-27 05:07:55 -0700

    prints a string, followed by a new line, to stdout

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* _puts - prints a string, followed by a new line
+* @s: tha string to be printed
+*
+* Description: prints a string, followed by a new line
+*
+*
+* Return: void
+*
+*/
+
+void (char *s)
+{
+	if (s == NULL || *s == '\0')
+	{
+		return;
+	}
+
+	while (s != '\0')
+	{
+		_putchar(*s);
+		s++;
+	}
+}
