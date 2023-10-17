c678550f8d4ba86a1c42e3643236cf32d1244239
Mina Sami <memo.samy0050@gmail.com>
2023-10-16 20:37:51 -0700

    returns the sum of all its parameters

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,30 +1,31 @@
-/*
- * File: 0-sum_them_all.c
- * Auth: Tolulope Fakunle
- */
-
+#include <stdio.h>
 #include "variadic_functions.h"
-#include <stdarg.h>
 
 /**
- * sum_them_all - Returns the sum of all its paramters.
- * @n: The number of paramters passed to the function.
- * @...: A variable number of paramters to calculate the sum of.
- *
- * Return: If n == 0 - 0.
- *         Otherwise - the sum of all parameters.
+ * sum_them_all - returns the sum of all its parameters
+ * @n: number of parameters
+ * Return: the sum of all of its parameters
  */
+
 int sum_them_all(const unsigned int n, ...)
 {
-	va_list nums;
-	unsigned int index, sum = 0;
+	unsigned int i;
+	int sum = 0;
+	va_list arg_list;
 
-	va_start(nums, n);
+	if (n == 0)
+	{
+		return (0);
+	}
+	
+	va_start(arg_list, n);
 
-	for (index = 0; index < n; index++)
-		sum += va_arg(nums, int);
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(arg_list, int);
+	}
 
-	va_end(nums);
+	va_end(arg_list);
 
 	return (sum);
 }
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,13 +1,6 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
 
-/*
- * File: variadic_functions.h
- * Auth: Tolulope Fakunle
- * Desc: Header file containing prototypes for all functions
- *       used in the 0x0F-variadic_functions directory.
- */
-
 #include <stdarg.h>
 
 /**
