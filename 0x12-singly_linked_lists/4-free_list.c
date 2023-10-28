3590d9d5c5ad84c8a24b36ee9c55d2afaa1af406
Mina Sami <memo.samy0050@gmail.com>
2023-10-28 14:06:14 -0700

    frees a list_t list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_list - frees a list_t list
+ * @head: the head pointer to the list
+ * Return: void
+ */
+
+void free_list(list_t *head)
+{
+	list_t *temp;
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp->str);
+		free(temp);
+	}
+	free(head);
+}
