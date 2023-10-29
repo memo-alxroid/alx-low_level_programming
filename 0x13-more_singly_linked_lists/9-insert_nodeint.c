d89c2cc81c40b086d8df3b7d4cf262b527badbdf
memo-alxroid <142922525+memo-alxroid@users.noreply.github.com>
2023-10-29 23:03:23 +0200

    Update 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -25,10 +25,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		return (NULL);
 	}
-
-	newNode->next = NULL;
-	newNode->n = n;
-
+	
 	i = 0;
 	if (idx != 0)
 	{
@@ -38,6 +35,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			i++;
 		}
 	}
+	newNode->n = n;
 	if (idx == 0)
 	{
 		newNode->next = *head;
