b92c783f5bfb709b0ca0b61c3e6effcd8bb69b3d
memo-alxroid <142922525+memo-alxroid@users.noreply.github.com>
2023-10-31 22:25:53 +0200

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,7 +18,7 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	fileDiscriptor = open(filename, O_RDWR | O_CREAT | O_TRUNC);
+	fileDiscriptor = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (fileDiscriptor == -1)
 	{
 		return (-1);
