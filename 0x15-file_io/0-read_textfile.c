ec664e9e2e89e0a7171c52d2d171100dae91a6b7
Mina Sami <memo.samy0050@gmail.com>
2023-10-31 11:54:09 -0700

    reads a text file and prints it to the POSIX standard output

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,36 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
 #include "main.h"
 
 /**
- * read_textfile - reads a text file and prints the letters
- * @filename: filename.
- * @letters: numbers of letters printed.
- *
- * Return: numbers of letters printed. It fails, returns 0.
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: file name string
+ * @letters: number of letters it should read and print
+ * Return: the actual number of letters it could read and print
  */
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	ssize_t nrd, nwr;
-	char *buf;
+	int fileDiscriptor;
+	char *text;
+	ssize_t numberOfByetsRead, numberOfByetsWrite;
 
-	if (!filename)
+	if (filename == NULL)
+	{
 		return (0);
-
-	fd = open(filename, O_RDONLY);
-
-	if (fd == -1)
+	}
+	fileDiscriptor = open(filename, O_CREAT, O_RDONLY);
+	if (fileDiscriptor == -1)
+	{
 		return (0);
-
-	buf = malloc(sizeof(char) * (letters));
-	if (!buf)
+	}
+	text = malloc(sizeof(char) * letters);
+	if (text == NULL)
+	{
 		return (0);
-
-	nrd = read(fd, buf, letters);
-	nwr = write(STDOUT_FILENO, buf, nrd);
-
-	close(fd);
-
-	free(buf);
-
-	return (nwr);
+	}
+	numberOfByetsRead = read(fileDiscriptor, text, letters);
+	if (numberOfByetsRead == -1)
+	{
+		free(text);
+		return (0);
+	}
+	numberOfByetsWrite = write(STDOUT_FILENO, text, numberOfByetsRead);
+	if (numberOfByetsWrite == -1)
+	{
+		free(text);
+		return (0);
+	}
+	free(text);
+	return (numberOfByetsWrite);
 }
