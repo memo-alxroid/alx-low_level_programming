de6207e3b5c9fd1d5a4222c23127dfa1dc4c695a
memo-alxroid <142922525+memo-alxroid@users.noreply.github.com>
2023-12-09 20:31:24 +0200

    Update 4-free_dlistint.c

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -12,6 +12,9 @@ void free_dlistint(dlistint_t *head)
 	dlistint_t *currentNode;
 	dlistint_t *nextNode;
 
+	if (head != NULL)
+		while (head->prev != NULL)
+			head = head->prev;
 	currentNode = head;
 	while (currentNode->next != NULL)
 	{
