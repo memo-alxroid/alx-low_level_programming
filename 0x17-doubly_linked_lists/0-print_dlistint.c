06158ec773c525fd6ec29303a0f4fbafbf7774b4
Mina Sami <memo.samy0050@gmail.com>
2023-12-09 07:34:23 -0800

    prints all the elements of a dlistint_t list

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -7,24 +7,23 @@
  * @h: head of the list
  * Return: the number of nodes
  */
+
 size_t print_dlistint(const dlistint_t *h)
 {
-	int count;
-
-	count = 0;
+	size_t numberOfNodes = 0;
 
 	if (h == NULL)
-		return (count);
-
-	while (h->prev != NULL)
-		h = h->prev;
+	{
+		return (0);
+	}
 
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
-		count++;
+		numberOfNodes++;
 		h = h->next;
 	}
 
-	return (count);
+	return (numberOfNodes);
+
 }
