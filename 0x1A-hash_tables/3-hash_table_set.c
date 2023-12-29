2f2600d021208490220fa9c5d962a9db787adcd4
memo-alxroid <142922525+memo-alxroid@users.noreply.github.com>
2023-12-29 21:36:25 +0200

    Update 3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -55,18 +55,17 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (ht == NULL)
 		return (0);
-
-	newNode = create_Node((char *)key, (char *)value);
-	if (newNode == NULL)
-		return (0);
-
 	keyIndex = key_index((const unsigned char *)key, ht->size);
 
+	newNode = create_Node((char *)key, (char *)value);
+
+	if (newNode == NULL)
+		return (0);
 	currentNode = ht->array[keyIndex];
 	if (currentNode == NULL)
 	{
+		newNode->next = ht->array[keyIndex];
 		ht->array[keyIndex] = newNode;
-		newNode->next = NULL;
 		return (1);
 	}
 	else
