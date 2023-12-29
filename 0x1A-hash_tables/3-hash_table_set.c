93e2877f5e174be6bc13fd8770cf2a7e3e63adad
memo-alxroid <142922525+memo-alxroid@users.noreply.github.com>
2023-12-29 21:27:13 +0200

    Update 3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -62,16 +62,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	keyIndex = key_index((const unsigned char *)key, ht->size);
 
-	for (i = keyIndex; ht->array[i]; i++)
-	{
-		if (strcmp(ht->array[i]->key, key) == 0)
-		{
-			free(ht->array[i]->value);
-			ht->array[i]->value = strdup(value);
-			return (1);
-		}
-	}
-
 	currentNode = ht->array[keyIndex];
 	if (currentNode == NULL)
 	{
